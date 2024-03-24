814f89216de25ca126b6f9c710ca44f7a696f160
James Abney <james.abney@gmail.com>
2024-03-23 20:32:28 -0500

    Color string generation

--- src/demo/system/color/color.ts
+++ src/demo/system/color/color.ts
@@ -14,12 +14,11 @@ const reset = `\x1b[0m`
 
 type Bits = 8 | 24
 
-const fg = (_: TemplateStringsArray, bits: Bits, code: number) => {
-    return `${esc}38;5;${code}m`
-}
+type Template = TemplateStringsArray
 
-const bg = (_: TemplateStringsArray, bits: Bits, code: number) => {
-    return `\x1b[48;5;${code}m`
+export interface ColorGenerator {
+    fg(t: Template): string
+    bg(t: Template): string
 }
 
 export interface IRgb {
@@ -30,35 +29,82 @@ export interface IRgb {
 
 export type RgbList = [r: number, g: number, b: number]
 
+const scale8 = Scale.domain([0, 255]).range([0, 5], Math.round)
+
+/**
+ * 16-231: 6 × 6 × 6 cube (216 colors):
+ *         16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
+ */
+const rgbToByte = (r: number, g: number, b: number) => {
+    return 16 + 36 * r + 6 * g + b
+}
+
+function bit8Gen(r: number, g: number, b: number): ColorGenerator {
+    const code = rgbToByte(scale8(r), scale8(g), scale8(b))
+    console.log(code)
+    return {
+        fg(t: Template) {
+            const [text = ''] = t
+            return `${esc}38;5;${code}m${text}`
+        },
+        bg(t: Template) {
+            const [text = ''] = t
+            return `${esc}48;5;${code}m${text}`
+        },
+    }
+}
+
+function bit24Gen(r: number, g: number, b: number): ColorGenerator {
+    console.log([r, g, b])
+    return {
+        fg(t: Template) {
+            const [text = ''] = t
+            return `${esc}38;2;${r};${g};${b}m${text}`
+        },
+        bg(t: Template) {
+            const [text = ''] = t
+            return `${esc}48;2;${r};${g};${b}m${text}`
+        },
+    }
+}
+
+const sanitize = (r: number, g: number, b: number): IRgb => {
+    return { r: Math.max(0, r) & 0xff, g: Math.max(0, g) & 0xff, b: Math.max(0, b) & 0xff }
+}
+
 export interface IColor {
     rgb(r: number, g: number, b: number): void
     gray(value: number): void
 }
 
-export class Color {
-    private static scaler = Scale.domain([0, 255]).range([0, 5], Math.round)
+export class Color<T extends Bits> {
+    static rgb<T extends Bits>(bits: T, r: number, g: number, b: number): Color<T> {
+        const s = sanitize(r, g, b)
 
-    static rgb(r: number, g: number, b: number, bits: Bits): Color {
-        r = Math.max(0, r) & 0xff
-        g = Math.max(0, g) & 0xff
-        b = Math.max(0, b) & 0xff
-
-        switch (bits) {
-            case 24:
-                return new Color(bits, r, g, b)
-            case 8: {
-                const scale = this.scaler
-                return new Color(bits, scale(r), scale(g), scale(b))
-            }
+        if (bits === 8) {
+            return new Color(bits, scale8(s.r), scale8(s.g), scale8(s.b))
+        } else {
+            return new Color(bits, s.r, s.g, s.b)
         }
     }
 
     constructor(
-        readonly bits: Bits,
+        readonly bits: T,
         readonly r: number,
         readonly g: number,
         readonly b: number
-    ) {}
+    ) {
+        const s = sanitize(r, g, b)
+        this.r = s.r
+        this.g = s.g
+        this.b = s.b
+    }
 
-    private toByte(value: number) {}
+    get str() {
+        if (this.bits === 8) {
+            return bit8Gen(this.r, this.g, this.b)
+        } else {
+            return bit24Gen(this.r, this.g, this.b)
+        }
+    }
 }
